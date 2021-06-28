98cf1c0453fc5933d2c9ac03915e51d03f20e7b3
Apoorva-Menon <66709872+Apoorva-Menon@users.noreply.github.com>
2021-06-28 22:45:03 +0530

    Update CountryNotFoundException.java

--- Day 1 - Session 1 and 2/spring-learn/src/main/java/com/cognizant/springlearn/exception/CountryNotFoundException.java
+++ Day 1 - Session 1 and 2/spring-learn/src/main/java/com/cognizant/springlearn/exception/CountryNotFoundException.java
@@ -6,7 +6,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Country Not Found")
 public class CountryNotFoundException extends Exception{
 	
-	/**
-	 * 
-	 */
 }
